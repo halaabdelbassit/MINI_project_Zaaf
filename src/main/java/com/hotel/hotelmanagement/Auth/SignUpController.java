e444dca1e299e4ae0a9faafc408042fc6c464539
ZAAFHachemrachid <rachidzaaf9@gmail.com>
2024-04-12 10:33:09 +0100

    Edit sing up

--- src/main/java/com/hotel/hotelmanagement/Auth/SignUpController.java
+++ src/main/java/com/hotel/hotelmanagement/Auth/SignUpController.java
@@ -54,11 +54,6 @@ public class SignUpController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         dbConnection = new DBConnection();
-        question.getItems().removeAll(question.getItems());
-        question.getItems().addAll("What is the name of your first pet?",
-                "What was your first car?",
-                "What elementary school did you attend?",
-                "What is the name of the town where you were born?");
     }
 
     @FXML
@@ -66,24 +61,18 @@ public class SignUpController implements Initializable {
         String name_text = name.getText();
         String username_text = username.getText();
         String password_text = password.getText();
-        String gender_text = getGender();
-        String question_text = question.getSelectionModel().getSelectedItem();
-        String answer_text = answer.getText();
         String address_text = address.getText();
 
         assert name_text != null;
         assert username_text != null;
         assert password_text != null;
-        assert gender_text != null;
-        assert question_text != null;
-        assert answer_text != null;
+
         assert address_text != null;
-        if (name_text.isEmpty() || username_text.isEmpty() || password_text.isEmpty() || gender_text.isEmpty()
-                || question_text.isEmpty() || answer_text.isEmpty() || address_text.isEmpty()) {
+        if (name_text.isEmpty() || username_text.isEmpty() || password_text.isEmpty()  || address_text.isEmpty()) {
             OptionPane("Every Field is required", "Error Message");
         } else {
-            String insert = "INSERT INTO users(name, username, password, gender, securityQuestion, answer, address)"
-                    + "VALUES (?, ?, ?, ?, ?, ?, ?)";
+            String insert = "INSERT INTO users(name, username, password, address)"
+                    + "VALUES (?, ?, ?, ?,)";
             connection = dbConnection.getConnection();
             try {
                 pst = connection.prepareStatement(insert);
@@ -94,10 +83,7 @@ public class SignUpController implements Initializable {
                 pst.setString(1, name_text);
                 pst.setString(2, username_text);
                 pst.setString(3, password_text);
-                pst.setString(4, gender_text);
-                pst.setString(5, question_text);
-                pst.setString(6, answer_text);
-                pst.setString(7, address_text);
+                pst.setString(4, address_text);
                 pst.executeUpdate();
                 OptionPane("Register Successfully", "Message");
             } catch (SQLException e) {
@@ -114,16 +100,6 @@ public class SignUpController implements Initializable {
         Scene scene = new Scene(root);
         login.setScene(scene);
         login.show();
-    }
-
-    public String getGender() {
-        String gender = "";
-        if (male.isSelected()) {
-            gender = "Male";
-        } else if (female.isSelected()) {
-            gender = "Female";
-        }
-        return gender;
     }
 
     private void OptionPane(String message, String title) {
